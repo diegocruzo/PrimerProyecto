5f359938e5c0693a8de3f7718781153c2edfa2ea
Diego <diegocruzo@gmail.com>
2022-07-07 18:43:17 -0500

    Proyecto personal finalizado, comprobar en PC.

--- webpersonal/portfolio/models.py
+++ webpersonal/portfolio/models.py
@@ -1,10 +1,18 @@
+from random import vonmisesvariate
 from django.db import models
 
 # Create your models here.
 class Project(models.Model):
-    title = models.CharField(max_length=200) # Campo de carácteres definido en los modelos de Django, es una cadena de caracteres en la base de datos
-    description = models.TextField() # Campo de texto más grande
-    image = models.ImageField() # Campo de imágen en la base de datos
-    created = models.DateTimeField(auto_now_add=True)  # Fecha de creación, se añadirá automáticamente cuando se cree la primera vez.
-    updated = models.DateTimeField(auto_now=True)  # Fecha de actualización o fecha de modificación, la diferencia respecto al anterior
-                                                    # es que este se ejecuta cada vez que se actualiza una instancia
+    title = models.CharField(max_length=200, verbose_name="Título") 
+    description = models.TextField(verbose_name="Descripción") 
+    image = models.ImageField(verbose_name="Imagen", upload_to="projects")
+    created = models.DateTimeField(auto_now_add=True, verbose_name="Fecha de creación")
+    updated = models.DateTimeField(auto_now=True, verbose_name="Fecha última modificación") 
+    
+    class Meta:
+        verbose_name = "proyecto"
+        verbose_name_plural = "proyectos"
+        ordering = ["-created"] 
+    
+    def __str__(self):
+        return self.title
